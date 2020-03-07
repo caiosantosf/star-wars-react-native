f1b44ca162f9e657629d98ff173b5e738931b17d
CAIO SANTOS FREITAS <22296@unifacef.edu.br>
2020-03-07 16:33:46 -0300

    Alteracoes da Auala

--- index.tsx
+++ index.tsx
@@ -3,17 +3,17 @@ import 'react-native-gesture-handler';
 import * as Stores from './src/stores';
 
 import { AppRegistry, SafeAreaView } from 'react-native';
-import { light as lightTheme, mapping } from '@eva-design/eva';
+import { dark as darkTheme, mapping } from '@eva-design/eva';
 
 import { ApplicationProvider } from '@ui-kitten/components';
 import { Provider } from 'mobx-react';
 import React from 'react';
-import Routes from './src/routes';
+import s from './src/s';
 import { name as appName } from './app.json';
 
 const ProviderConfigured = () => (
     <Provider {...Stores}>
-        <ApplicationProvider mapping={mapping} theme={lightTheme}>
+        <ApplicationProvider mapping={mapping} theme={darkTheme}>
             <SafeAreaView style={{ flex: 1 }}>
                 <Routes />
             </SafeAreaView>
